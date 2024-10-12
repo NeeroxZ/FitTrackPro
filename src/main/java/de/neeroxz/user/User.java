236760faf84b27bf1b4c55ccd4fb0527dcb028eb
NeeroxZ <neeroxz@hotmail.de>
2024-10-12 19:23:16 +0200

    feat: USER Record

--- src/main/java/de/neeroxz/user/User.java
+++ src/main/java/de/neeroxz/user/User.java
@@ -6,5 +6,51 @@ package de.neeroxz.user;
  * @author NeeroxZ
  * @date 01.10.2024
  */
-public class User {
+import de.neeroxz.util.PasswordHasher;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public record User(
+        String firstName,
+        String lastName,
+        String username,
+        String gender,
+        double weight,
+        double height,
+        LocalDate birthDate,
+        String hashedPassword
+) {
+
+    // Berechnung des Alters aus dem Geburtsdatum
+    public int getAge() {
+        return Period.between(this.birthDate, LocalDate.now()).getYears();
+    }
+
+    // Benutzerinformationen anzeigen
+    public void printUserInfo() {
+        System.out.println("Benutzer: " + firstName + " " + lastName);
+        System.out.println("Alter: " + getAge() + " Jahre");
+        System.out.println("Größe: " + height + " cm");
+        System.out.println("Gewicht: " + weight + " kg");
+        System.out.println("Geschlecht: " + gender);
+        System.out.println("Benutzername: " + username);
+    }
+
+    // Static factory method to create a User with hashed password
+    public static User createUserWithHashedPassword(
+            String firstName,
+            String lastName,
+            String username,
+            String gender,
+    double weight,
+    double height,
+    LocalDate birthDate,
+    String password,
+    PasswordHasher passwordHasher
+    ) throws Exception {
+        String hashedPassword = passwordHasher.hash(password);  // Passwort verschlüsseln
+        return new User(firstName, lastName, username, gender, weight, height, birthDate, hashedPassword);
+    }
 }
+
