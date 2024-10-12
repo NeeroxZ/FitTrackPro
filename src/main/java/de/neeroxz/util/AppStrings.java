66f1616788bffadc9cb45d4b6daf5b60e44a5ec0
NeeroxZ <neeroxz@hotmail.de>
2024-10-12 13:27:48 +0200

    feat: Enum AppStrings hinzugefügt für ASCII-Art und Linienseparatoren

---
+++ src/main/java/de/neeroxz/util/AppStrings.java
@@ -0,0 +1,29 @@
+package de.neeroxz.util;
+
+/**
+ * Enum: AppStrings
+ *
+ * @author NeeroxZ
+ * @date 12.10.2024
+ */
+public enum AppStrings {
+    ASCIIART("  ______ _           _____                 _____                     \n" +
+            " |  ____| |         |_   _|               |  __ \\                    \n" +
+            " | |__  | |__   __ _| |   _ __   ___ _ __ | |  | | ___ _ __ ___  ___ \n" +
+            " |  __| | '_ \\ / _` | | | | '_ \\ / _ \\ '_ \\| |  | |/ _ \\ '__/ __|/ _ \\\n" +
+            " | |____| | | | (_| | | | | | | |  __/ | | | |__| |  __/ |  \\__ \\  __/\n" +
+            " |______|_| |_|\\__,_| |_| |_| |_|\\___|_| |_|_____/ \\___|_|  |___/\\___|\n"
+    ),
+    LINESEPARATOR("------------------------------------------");
+
+    private final String s;
+
+    AppStrings(String s) {
+        this.s = s;
+    }
+
+    // Getter-Methode
+    public String getAppString() {
+        return s;
+    }
+}
